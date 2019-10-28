0f5d636ad56bb6177cb71987ab3fc5a08fb921c6
Dillon2332 <dillonduff1@frontier.com>
2019-10-28 10:17:15 -0400

    Chapter 5 uploaded.

--- Chapter_5/Lopez_TIY5.10.py
+++ Chapter_5/Lopez_TIY5.10.py
@@ -1,7 +1,10 @@
 current_users = ['user1', 'user2', 'user3', 'user4', 'user5']
+
 new_users = ['new_user1', 'USer1', 'user3', 'new_user2']
+
 for new_user in new_users:
     if new_user.lower() in current_users:
         print(f"Sorry, the username: {new_user.lower()} is taken. Please try another.")
     else:
         print(f"Good news! The username: {new_user.lower()} is available.")
+
