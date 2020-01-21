90ed4cfc57f17a79408f767e2ba6720b26e41dc7
Manuel Lopez <cozarzepol123@gmail.com>
2020-01-21 11:31:31 -0500

    ..

--- Chapter_17/python_repos.py
+++ Chapter_17/python_repos.py
@@ -1,6 +1,5 @@
 import requests
 
-
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
@@ -11,23 +10,25 @@ def get_request():
     return requests.get(url, headers=headers)
 
 
-r = get_request()
+if __name__ == '__main__':
 
-# Store API response in a variable.
-response_dict = r.json()
+    r = get_request()
 
-print(f"Total repositories: {response_dict['total_count']}")
+    # Store API response in a variable.
+    response_dict = r.json()
 
-# Explore information about the repositories.
-repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+    print(f"Total repositories: {response_dict['total_count']}")
 
-print("\nSelected information about first repo:")
-for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Created: {repo_dict['created_at']}")
-    print(f"Updated: {repo_dict['updated_at']}")
-    print(f"Description: {repo_dict['description']}")
+    # Explore information about the repositories.
+    repo_dicts = response_dict['items']
+    print(f"Repositories returned: {len(repo_dicts)}")
+
+    print("\nSelected information about first repo:")
+    for repo_dict in repo_dicts:
+        print(f"Name: {repo_dict['name']}")
+        print(f"Owner: {repo_dict['owner']['login']}")
+        print(f"Stars: {repo_dict['stargazers_count']}")
+        print(f"Repository: {repo_dict['html_url']}")
+        print(f"Created: {repo_dict['created_at']}")
+        print(f"Updated: {repo_dict['updated_at']}")
+        print(f"Description: {repo_dict['description']}")
